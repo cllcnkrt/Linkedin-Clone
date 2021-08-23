254d2f989b7512cdc58af14de82c87f3d73d78b2
cllcnkrt <cllcnkrt@outlook.com>
2021-08-23 16:42:39 +0300

    post will start

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from '@material-ui/icons/Create';
-
+import InputOption from './InputOption/InputOption';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 function Feed() {
     return (
         <div className="feed">
@@ -14,7 +18,11 @@ function Feed() {
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                    <InputOption Icon={CalendarTodayIcon} title="Write acticle" color="#7FC15E"/>
+
                 </div>
             </div>
         </div>
