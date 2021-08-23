f5e006f678dc5b5fd1636c7c30590dd06ecfa6a4
cllcnkrt <cllcnkrt@outlook.com>
2021-08-23 13:23:48 +0300

    headerOption done, sidebar will start

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,10 @@ import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from '../HeaderOption/HeaderOption';
 
 import HomeIcon from '@material-ui/icons/Home';
-
-
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 function Header() {
     return (
         <div className="header">
@@ -18,7 +20,12 @@ function Header() {
            </div>
 
            <div className="header__right">
-                <HeaderOption />
+                <HeaderOption title="Home" Icon={HomeIcon}  />
+                <HeaderOption title="My Network" Icon={SupervisorAccountIcon}  />
+                <HeaderOption title="Jobs" Icon={BusinessCenterIcon}  />
+                <HeaderOption title="Messaging" Icon={ChatIcon}  />
+                <HeaderOption title="Notifications" Icon={NotificationsIcon}  />
+                <HeaderOption avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Brad_Pitt_2019_by_Glenn_Francis.jpg/1200px-Brad_Pitt_2019_by_Glenn_Francis.jpg" title="me" />
            </div>
         </div>
     )
--- src/components/HeaderOption/HeaderOption.js
+++ src/components/HeaderOption/HeaderOption.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
-function HeaderOption() {
+import "./HeaderOption.css"
+import Avatar from '@material-ui/core/Avatar';
+function HeaderOption({title,Icon, avatar}) {
     return (
         <div className="headerOption">
-            
+            {Icon && <Icon className="headerOption__icon" />}
+            {avatar && (
+                <Avatar className="headerOption__icon" src={avatar}/>
+            )}
+            <h3 className="headerOption__title">{title}</h3>
         </div>
     )
-}
+} 
 
 export default HeaderOption
