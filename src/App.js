97ea758aed745a6469ca59b9177c38dca7bdd97c
cllcnkrt <cllcnkrt@outlook.com>
2021-08-24 20:34:44 +0300

    login page added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Feed from "./components/Feed/Feed";
+import {selectUser} from "./features/userSlice";
+import Login from "./components/Login/Login";
+import { useSelector } from "react-redux";
+
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
-      {/* Header */}
       <Header />
-      <div className="app__body">
-        {/* App body */}
-        <Sidebar />
-        {/* feed */}
-        <Feed/>
-        {/* widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* widgets */}
+        </div>
+      )}
     </div>
   );
 }
